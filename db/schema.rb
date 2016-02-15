a2fd4c264ea1ab28685443badecc8c3eeeccf31d
rikukawano <Riku@Rikus-MacBook-Pro.local>
2016-02-15 20:23:04 +0900

    Styling index page

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210144938) do
+ActiveRecord::Schema.define(version: 20160211023701) do
 
   create_table "ideas", force: :cascade do |t|
-    t.text     "ideas"
+    t.text     "description"
     t.string   "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
